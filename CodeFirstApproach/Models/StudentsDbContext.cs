7565c5869281ee86dcc4912e03a6eb015700ac6f
AtanasG6 <gyulchev666@gmail.com>
2026-04-12 23:56:10 +0300

    Add Fluent API configuration for Student entity

--- CodeFirstApproach/Models/StudentsDbContext.cs
+++ CodeFirstApproach/Models/StudentsDbContext.cs
@@ -10,6 +10,18 @@ public class StudentsDbContext : DbContext
             UseSqlServer("Server=.;Database=StudentsDb;Integrated Security=True;TrustServerCertificate=True;");
     }
 
+    override protected void OnModelCreating(ModelBuilder modelBuilder)
+    {
+
+        // Fluent API configuration
+        modelBuilder.Entity<Student>(e =>
+        {
+            e.Property(x => x.LastName)
+                .IsRequired()
+                .HasMaxLength(50);
+        });
+    }
+
     public DbSet<Student> Students { get; set; }
     public DbSet<Course> Courses { get; set; }
     public DbSet<Grade> Grades { get; set; }
