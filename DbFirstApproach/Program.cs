bfa9d3da7d7b38e185d1898bb74b367abbca721b
AtanasG6 <gyulchev666@gmail.com>
2026-04-12 23:29:56 +0300

    Add DB First approach demo using EF Core scaffold

---
+++ DbFirstApproach/Program.cs
@@ -0,0 +1,15 @@
+﻿using DbFirstApproach.Models;
+
+namespace DbFirstApproach;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        // dotnet ef dbcontext scaffold "Server=.;Database=Softuni;Integrated Security=True;TrustServerCertificate=True;" Microsoft.EntityFrameworkCore.SqlServer -o Models
+        var dbContext = new SoftuniContext();
+        var employees = dbContext.Employees.ToList();
+        foreach (var employee in employees) 
+            Console.WriteLine($"{employee.FirstName} {employee.LastName}");
+    }
+}
