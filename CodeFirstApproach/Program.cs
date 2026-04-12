9c9c0af413773a2ef7f3581c90090fccb6ac2c94
AtanasG6 <gyulchev666@gmail.com>
2026-04-13 00:27:03 +0300

    Add ChangeTracker comment and second course update example

--- CodeFirstApproach/Program.cs
+++ CodeFirstApproach/Program.cs
@@ -14,13 +14,17 @@ public class Program
             );
 
         // ! - null forgiving operator, because we are sure that there is a course with Id = 1
-        Course course = dbContext.Courses.FirstOrDefault(c => c.Id == 1)!;
-        course.Name = "Atanas changed the name!!!";
+        Course course1 = dbContext.Courses.FirstOrDefault(c => c.Id == 1)!;
+        course1.Name = "Atanas changed the name!!!";
 
-        dbContext.Courses.Remove(course);
+        dbContext.Courses.Remove(course1);
 
         // If EF Core does not support sth like RANK, ROW_NUMBER, etc., we can always execute raw SQL queries 
         // dbContext.Database.ExecuteSqlRaw("DELETE FROM Courses WHERE Id = 1");
+
+        // The ChangeTracker will track the changes we made to course1 and will generate the appropriate SQL commands to update the database when we call SaveChanges()
+        Course course2 = dbContext.Courses.FirstOrDefault(c => c.Id == 2)!;
+        course2.Name = "Atanas changed the name!!!";
 
         dbContext.SaveChanges();
     }
